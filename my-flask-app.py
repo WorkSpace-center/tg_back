a22c65628ef39119033d50438b7ae8e34d05a1cc
WorkSpace-center <evgenrv28@gmail.com>
2025-01-26 21:06:57 +0300

    second commit

--- my-flask-app.py
+++ my-flask-app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
-app = Flask("MyFlaskApp" )
+app = Flask("MyFlaskApp")
 
-@app.route('/', methods=['GET'])
-def welcome ():
-    return "<h1>Hello World</h1>"
-if __name__ == '__main__':
-    app.run ()
+
+@app.route("/", methods=["GET"])
+def welcome():
+    return "<h1>Hello World !!!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
