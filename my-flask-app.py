491941017ebf368dcf8ecc9c9b4f72fd836f24b2
WorkSpace-center <evgenrv28@gmail.com>
2025-01-26 22:47:24 +0300

    fourth commit

--- my-flask-app.py
+++ my-flask-app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+#from flask import Flask
+
+#app = Flask("MyFlaskApp")
+
+#@app.route("/", methods=["GET"])
+#def welcome():
+#    return "<h1>Hello motherfacker !!!</h1>"
+
+from flask import Flask, request
 
 app = Flask("MyFlaskApp")
 
-
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def welcome():
+    if request.method == "POST":
+        # Получите данные из POST-запроса
+        data = request.json  # Если данные отправляются в формате JSON
+        # Извлекаем нужные данные (например, значение по ключу 'message')
+        message = data.get("message", "No message provided")
+        return f"<h1>Hello motherfacker !!! {message}</h1>"
     return "<h1>Hello motherfacker !!!</h1>"
-
 
 if __name__ == "__main__":
     app.run()
